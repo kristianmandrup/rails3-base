2998e30724e12d2c4effc5383e31519b974d66cb
Carl Sverre <carl@carlsverre.com>
2010-04-11 20:41:35 -0700

    Finished rails 3 reset

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.cookie_secret = 'fc2be700138556dcda7c56426cfcd92e750b1eff51441b6002329e2dba2410891009a0bf41456868e052cb1e1aa9e259a3cd72893798478c5c88da71a9dc18be'
+Rails.application.config.cookie_secret = '66c372260345a06c5fd5780cb6994c078d33b8e7148e84fae8bd1067d09773f5c775b52cedcb9b0c25a61e788511bf10354835a8e4973416f9b65fd43d8cad67'
